a2570297822abb914011e3082bf60d125b2155d2
Duane-Njenga <duanenjenga@gmail.com>
2026-04-24 12:09:01 +0300

    Added Movies Menu

--- src/MoviesApp.java
+++ src/MoviesApp.java
@@ -10,20 +10,22 @@ import javafx.scene.layout.GridPane;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
-public class GenresApp extends Application {
+public class MoviesApp extends Application {
 
     @Override
     public void start(Stage stage) {
 
-        Text pgTitle = new Text("Movie Genres Menu");
+        Text pgTitle = new Text("Movies Menu");
         pgTitle.setStyle("-fx-font: normal bold 20px 'serif'; -fx-alignment: center");
 
-        Text text1 = new Text("Name:");
-        Text text2 = new Text("Registered:");
-        TextField textField1 = new TextField();
-        ComboBox<String> comboBox = new ComboBox<>();
-        Button button1 = new Button("Save");
-        Button button2 = new Button("Remove");
+        Text text1 = new Text("Genres:");
+        Text text2 = new Text("Name:");
+        Text text3 = new Text("Registered:");
+        TextField name = new TextField();
+        ComboBox<String> genre = new ComboBox<>();
+        ComboBox<String> registered = new ComboBox<>();
+        Button button1 = new Button("Save Movie");
+        Button button2 = new Button("Remove Movie");
 
         GridPane gridPane = new GridPane();
         gridPane.setMinSize(600, 400);
@@ -37,12 +39,14 @@ public class GenresApp extends Application {
 
         gridPane.add(pgTitle, 0, 0);
         gridPane.add(text1, 0, 1);
-        gridPane.add(textField1, 1, 1);
-        gridPane.add(button1, 1, 2);
+        gridPane.add(text2, 0, 2);
+        gridPane.add(text3, 0, 4);
 
-        gridPane.add(text2, 0, 3);
-        gridPane.add(comboBox, 1, 3);
-        gridPane.add(button2, 1, 4);
+        gridPane.add(genre, 1, 1);
+        gridPane.add(name, 1, 2);
+        gridPane.add(button1, 1, 3);
+        gridPane.add(registered, 1, 4);
+        gridPane.add(button2, 1, 5);
 
         String btnStyle = "-fx-background-color: CORNFLOWERBLUE; -fx-text-fill: white; -fx-font-size: 13pt; -fx-pref-width: 200px;-fx-background-radius: 8px;";
         String hoverStyle = "-fx-background-color: darkslateblue;-fx-text-fill: white; -fx-font-size: 13pt;-fx-pref-width: 200px;-fx-background-radius: 8px;";
@@ -53,15 +57,18 @@ public class GenresApp extends Application {
             btn.setOnMouseExited(e ->  btn.setStyle(btnStyle));
         }
 
-        comboBox.setStyle("-fx-pref-width: 200px");
+        genre.setStyle("-fx-pref-width: 200px");
+        registered.setStyle("-fx-pref-width: 200px");
         text1.setStyle("-fx-font: normal bold 20px 'serif'");
         text2.setStyle("-fx-font: normal bold 20px 'serif'");
+        text3.setStyle("-fx-font: normal bold 20px 'serif'");
+
         gridPane.setStyle("-fx-background-color: White;");
 
         Scene scene = new Scene(gridPane);
 
 
-        stage.setTitle("Genres");
+        stage.setTitle("Movies Menu");
         stage.setScene(scene);
         stage.show();
     }
